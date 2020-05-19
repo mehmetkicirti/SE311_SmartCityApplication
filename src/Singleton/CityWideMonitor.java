5aad69ea58e78e32ffd5b13a7a41996b3af6f5d4
mehmetkicirti <mehmetkicirti@hotmail.com>
2020-05-20 00:06:14 +0300

    Unnecessary codes deleted on patterns

--- src/Singleton/CityWideMonitor.java
+++ src/Singleton/CityWideMonitor.java
@@ -1,11 +1,5 @@
 package Singleton;
 
-import Composite.Apartment;
-import Composite.INeighborhood;
-import Composite.NeighborhoodComposite;
-import Composite.Pole;
-import FactoryMethod.SensorCreator;
-import FactoryMethod.Utils.SensorType;
 import Iterator.CityCollection;
 import Iterator.ICityIterator;
 
@@ -16,7 +10,6 @@ public class CityWideMonitor {
 
     private CityWideMonitor(){
         _cityCollection = new CityCollection();
-        //dummyCreateCityCollection();
     }
 
     public static CityWideMonitor getCityWideMonitor(){
@@ -27,40 +20,6 @@ public class CityWideMonitor {
     }
     public CityCollection getCityCollection(){
         return _cityCollection;
-    }
-
-
-/*
-
-    private List<Sensor> dummyCreateSensorsApartment(){
-        ArrayList<Sensor> apartmentSensors = new ArrayList<>();
-        apartmentSensors.add(SensorCreator.createSensor(SensorType.Pollution,"Pollution Sensor",12));
-        apartmentSensors.add(SensorCreator.createSensor(SensorType.Congestion,"Congestion Sensor",22));
-        apartmentSensors.add(SensorCreator.createSensor(SensorType.Noise,"Noise Sensor",23));
-        apartmentSensors.add(SensorCreator.createSensor(SensorType.Temperature,"Temperature Sensor",25));
-        return apartmentSensors;
-    }
-
-    private List<Sensor> dummyCreateSensorsPole(){
-        ArrayList<Sensor> poleSensors = new ArrayList<>();
-        poleSensors.add(SensorCreator.createSensor(SensorType.Pollution,"Pollution Sensor",12));
-        poleSensors.add(SensorCreator.createSensor(SensorType.Congestion,"Congestion Sensor",22));
-        poleSensors.add(SensorCreator.createSensor(SensorType.Noise,"Noise Sensor",23));
-        poleSensors.add(SensorCreator.createSensor(SensorType.Temperature,"Temperature Sensor",25));
-        return poleSensors;
-    }
-
-    private void dummyCreateCityCollection(){
-        NeighborhoodComposite eylulMah = new NeighborhoodComposite("Eylül Neighborhood");
-        NeighborhoodComposite ulastirmaStreet = new NeighborhoodComposite("Ulastirma Street");
-        eylulMah.add(ulastirmaStreet);
-        ulastirmaStreet.add(new Apartment("Tunay Apartment",dummyCreateSensorsApartment()));
-        ulastirmaStreet.add(new Pole("Tunay Apartment belonging the Pole",dummyCreateSensorsPole()));
-        _cityCollection.addItem(eylulMah);
-    }
- */
-    public void addNeighborhood(INeighborhood neighborhood){
-        _cityCollection.addItem(neighborhood);
     }
 
     public int getMalfuctioningSensorsNumber(){
